c92e292d8f5afb56af6cf621abf7c7dbfecfa4c2
Tom <TD5@users.noreply.github.com>
2023-05-05 15:09:48 +0100

    Put bot onto master to validate comment-oriented workflow

---
+++ .github/scripts/run-fuzzer.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+FUZZER_DIR=${1}
+OTP_DIR=${2}
+
+
+set -euxo pipefail
+
+# Docker host mount the OTP dir here
+OUT=/github/
+
+# Install Rust non-interactively
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain nightly
+PATH=$HOME/.cargo/bin:$PATH
+sudo apt-get install parallel
+
+mkdir "../${FUZZER_DIR}"
+cd "../${FUZZER_DIR}"
+git clone https://github.com/WhatsApp/erlfuzz.git
+cd erlfuzz
+
+# Be permissive when building erlfuzz - we don't need it to we warning-free for
+# erlfuzz to be useful
+RUSTFLAGS=-Awarnings $HOME/.cargo/bin/cargo build --release
+
+mkdir -p out
+mkdir -p interesting
+mkdir -p minimized
+
+#N=100000
+N=5 # TODO Remove after testing
+
+echo "Fuzzing erl"
+echo "Generating ${N} test cases"
+
+seq ${N} | parallel --line-buffer "./target/release/erlfuzz fuzz-and-reduce -c ./run_erl_once.sh --tmp-directory out --interesting-directory interesting --minimized-directory minimized test{}"
+
+echo "Fuzzing complete"
+
+mv minimized "${OUT}"/minimized
+
+echo "Results written to: ${OUT}/minimized"
