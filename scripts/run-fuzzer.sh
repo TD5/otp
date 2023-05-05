53d332716c99f3286b73b596148f80ac930355cd
Tom <TD5@users.noreply.github.com>
2023-05-05 15:09:48 +0100

    Put bot onto master to validate comment-oriented workflow

---
+++ scripts/run-fuzzer.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+FUZZER_DIR=${1}
+OTP_DIR=${2}
+
+
+set -euxo pipefail
+
+# Install Rust non-interactively
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
+PATH=~/.cargo/bin:$PATH
+
+mkdir "../${FUZZER_DIR}"
+cd "../${FUZZER_DIR}"
+git clone https://github.com/WhatsApp/erlfuzz.git
+cd erlfuzz
+
+cargo build --release
+
+mkdir -p out
+mkdir -p interesting
+mkdir -p minimized
+
+#N=100000
+N=5 # TODO Remove after testing
+
+echo "Fuzzing erl"
+echo "Generating ${N} test cases"
+
+seq ${N} | parallel --line-buffer "./target/release/erlfuzz fuzz -c ./run_erl_once.sh --tmp-directory out --interesting-directory interesting --minimized-directory minimized test{}"
+
+
+echo "Fuzzing complete"
